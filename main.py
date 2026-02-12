e8d214e6a65ffd45c1958eeffdbdc54ca9b8478f
Malchfer <chrippa.m@gmail.com>
2026-02-12 11:08:28 +0100

    added circleshape.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # fps
+    clock = pygame.time.Clock()
+    dt = 0
+
     # game loop
     while True:
         log_state()
@@ -18,6 +22,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
